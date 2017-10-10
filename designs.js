0a0f7ab69d326410565435a75c7ae6b9355046e9
Bart <kluczynskib@gmail.com>
2017-10-10 12:21:28 +0100

    applied suggested changes/refactored the code

--- designs.js
+++ designs.js
@@ -1,48 +1,31 @@
 // Select width and height input
-function getHeightWidth() {
+function outputGrid() {
     let inputHeight = document.getElementById('input_height').value
     let inputWidth = document.getElementById('input_width').value
-    let canvas = document.getElementById('pixel_canvas')
-    canvas.innerHTML = makeGrid(inputWidth, inputHeight);
-}
-//add click listeners to submit button, colour picked and colour approver
-let input = document.getElementById('subButton');
-input.addEventListener('click', getHeightWidth, false)
-let pixel_canvas = document.getElementById('pixel_canvas');
-pixel_canvas.addEventListener('click', applyColor, false)
-let approver = document.getElementById('approver');
-approver.addEventListener('click', applyColor, false)
-
-//apply chosen color
-function applyColor() {
-    let color = getColour();
-    let trs = document.getElementsByTagName("td")
-    let trsCount = trs.length - 1;
-    let cell;
-    for (let i = 0; i <= trsCount; i++) {
-        trs[i].onclick = function(e) {
-            cell = ("this is", document.getElementById(this.id));
-            cell.style.backgroundColor = color;
-        }
-    }
+    let table = document.getElementById('pixel_canvas');
+    let colorPicker = document.getElementById("colorPicker");
+    constructGrid(inputHeight, inputWidth, table, colorPicker);
 }
 
-// return colour value
-function getColour() {
-    let color = document.getElementById('colorPicker').value
-    return color
-}
+//outputting grid on submit
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(e) {
+    e.preventDefault();
+    outputGrid();
+});
 
 //return grid
-function makeGrid(row, column) {
-    let id = row * column - 1;
-    let grid = '';
-    for (let r = 0; r < row; r++) {
-        grid += '<tr>'
-        for (let c = 0; c < column; c++) {
-            grid += `<td id=${id--}></td>`;
+function constructGrid(rows, columns, table, colorPicker) {
+    table.innerHTML = "";
+    for (let i = 0; i < rows; i++){
+      //insert n-th rows into the table_id
+      const row = table.insertRow(i)
+      for (let j = 0; j < columns; j++){
+        const cell = row.insertCell(j)
+        cell.onclick = () => {
+          cell.style.backgroundColor = colorPicker.value
         }
-        grid += '</tr>'
+      }
     }
-    return grid
-}
+    return table
+  }
